7fb78c09051974c172f179dc13fa290412894627
Newline <JordanPatel@pm.me>
2020-05-18 00:31:19 +0200

    Added burpsuite to default installables

--- kalidesktopdocker/post.sh
+++ kalidesktopdocker/post.sh
@@ -1,6 +1,6 @@
 export DEBIAN_FRONTEND=noninteractive
 apt-get update
-apt-get install seclists -y
+apt-get install seclists burpsuite -y
 apt-get dist-upgrade -y
 sl /usr/share/seclists /usr/share/wordlists/seclists
 sl /usr/share/wordlists /root/wordlists
