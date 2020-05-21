837dfb168f1075033fa119811e3db670b17aca3f
Newline <JordanPatel@pm.me>
2020-05-21 22:01:59 +0200

    Changed post load order

--- kalidesktopdocker/startup.sh
+++ kalidesktopdocker/startup.sh
@@ -2,11 +2,11 @@
 
 export DISPLAY=:1
 Xvfb :1 -screen 0 1920x1080x16 &
-chmod +x post.sh
-./post.sh &>/dev/null &
 sleep 5
 plasmashell  &>/dev/null &
 kwin --replace &>/dev/null & 
 sleep 5
+chmod +x /root/post.sh
+/root/post.sh &>/dev/null &
 x11vnc -display :1 -nopw -listen localhost -xkb -forever & # -ncache 10 -ncache_cr can be added for performance. Might give issues with some vnc viewers
 cd /root/noVNC && ln -s vnc_auto.html index.html && ./utils/launch.sh --vnc localhost:5900
