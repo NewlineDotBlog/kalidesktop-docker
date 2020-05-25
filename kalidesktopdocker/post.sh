412a60b5311f01d533298e13eb1829246ab68790
NewlineDotBlog <jordanpatel@pm.me>
2020-05-25 22:09:55 +0200

    Big updates, integrated sh scripts in dockerfile

--- kalidesktopdocker/post.sh
+++
@@ -1,7 +0,0 @@
-export DEBIAN_FRONTEND=noninteractive
-apt-get update
-apt-get install seclists burpsuite -y
-apt-get dist-upgrade -y
-sl /usr/share/seclists /usr/share/wordlists/seclists
-sl /usr/share/wordlists /root/wordlists
-unset DEBIAN_FRONTEND
