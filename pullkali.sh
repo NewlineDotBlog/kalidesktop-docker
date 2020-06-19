5835179d811911d2e057b41dafbf1bf98a3e170a
Newline <JordanPatel@pm.me>
2020-06-19 22:33:00 +0200

    Added connection menu

--- pullkali.sh
+++ pullkali.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# This script is meant for pulling the image from dockerhub. This is recommended to get started as fast as possible.
+
 # Add any personal modifications here.
 # After setting up the base image, this will be applied and committed to a new image
 # This way, you can download the prebuilt image from dockerhub, yet apply any personal modifications to the image
@@ -34,4 +36,29 @@ unset contname
 
 echo 'Starting personalized container'
 sudo docker run -d --network host --privileged -v $HOME:/home/$USER --mount source=kalivol,target=/opt/vol/ --name 'kalidesktop' kalidesktop
+sleep 5
+
+echo 'How do you want to connect?'
+select CHOICE in vnc novnc ssh none ; do
+
+  case $CHOICE in
+    vnc)
+        echo 'Opening your default vnc viewer...'
+        xdg-open vnc://localhost:5900 & || echo 'Opening vnc viewer failed. Please install a vnc viewer and connect to localhost:5900'
+        ;;
+    novnc)
+        echo 'Opening your default browser...'
+        xdg-open http://127.0.0.1:6080/vnc.html &
+        ;;
+    ssh)
+        echo 'Open a root shell with the following command, password "toor"'
+        echo 'ssh root@localhost -p 22000 -X'
+        ;;
+    none)
+        ;;
+    *)
+  esac
+  break
+done
+echo 'All done! Happy hacking!'
 
