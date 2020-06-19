027eebe92e2ab8fe1edaa0660419b0143441c80c
Newline <JordanPatel@pm.me>
2020-06-19 23:00:50 +0200

    Minor edit

--- pullkali.sh
+++ pullkali.sh
@@ -7,7 +7,7 @@
 # This way, you can download the prebuilt image from dockerhub, yet apply any personal modifications to the image
 # that will be stored in a new reusable image.
 # Use '&&' or ';' for separate commands.
-mods='apt-get update && apt-get install nmap'
+mods='export DEBIAN_FRONTEND=noninteractive && apt-get update && apt-get install nmap'
 
 echo 'Pulling the latest image from dockerhub, this might take a few minutes...'
 sudo docker pull newlinedotblog/kalidesktop
@@ -20,7 +20,7 @@ sudo docker image tag newlinedotblog/kalidesktop kalidesktop-base
 
 echo 'Starting the base container...'
 contname=`sudo docker run -d --network host --privileged -v $HOME:/home/$USER --mount source=kalivol,target=/opt/vol/ kalidesktop-base`
-sleep 5
+sleep 3
 
 echo 'Running personal modifications'
 sudo docker exec -it $contname /bin/bash -c "$mods"
@@ -30,12 +30,13 @@ sudo docker commit $contname kalidesktop
 
 echo 'Stopping base container'
 sudo docker stop $contname
+sleep 5
 
 unset mods
 unset contname
 
 echo 'Starting personalized container'
-sudo docker run -d --network host --privileged -v $HOME:/home/$USER --mount source=kalivol,target=/opt/vol/ --name 'kalidesktop' kalidesktop
+sudo docker run --network host --privileged -v $HOME:/home/$USER --mount source=kalivol,target=/opt/vol/ kalidesktop
 sleep 5
 
 echo 'How do you want to connect?'
@@ -44,7 +45,7 @@ select CHOICE in vnc novnc ssh none ; do
   case $CHOICE in
     vnc)
         echo 'Opening your default vnc viewer...'
-        xdg-open vnc://localhost:5900 & || echo 'Opening vnc viewer failed. Please install a vnc viewer and connect to localhost:5900'
+        xdg-open vnc://localhost:5900  || echo 'Opening vnc viewer failed. Please install a vnc viewer and connect to localhost:5900' & 
         ;;
     novnc)
         echo 'Opening your default browser...'
