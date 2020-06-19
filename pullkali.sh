01f4cba9512f93dac052a1ae0abdd19b9e15733f
Newline <JordanPatel@pm.me>
2020-06-19 22:07:31 +0200

    Moved personalized commands to variable

--- pullkali.sh
+++ pullkali.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Add any personal modifications here.
+# After setting up the base image, this will be applied and committed to a new image
+# This way, you can download the prebuilt image from dockerhub, yet apply any personal modifications to the image
+# that will be stored in a new reusable image.
+# Use '&&' or ';' for separate commands.
+mods='apt-get update && apt-get install nmap'
+
 echo 'Pulling the latest image from dockerhub, this might take a few minutes...'
 sudo docker pull newlinedotblog/kalidesktop
 
@@ -14,7 +21,7 @@ contname=`sudo docker run -d --network host --privileged -v $HOME:/home/$USER --
 sleep 5
 
 echo 'Running personal modifications'
-sudo docker exec -it $contname /bin/bash -c 'apt-get update && apt-get install nmap'
+sudo docker exec -it $contname /bin/bash -c "$mods"
 
 echo 'Committing changes'
 sudo docker commit $contname kalidesktop
@@ -22,6 +29,9 @@ sudo docker commit $contname kalidesktop
 echo 'Stopping base container'
 sudo docker stop $contname
 
+unset mods
+unset contname
+
 echo 'Starting personalized container'
-sudo docker run -d --network host --privileged -v $HOME:/home/$USER --mount source=kalivol,target=/opt/vol/ kalidesktop
+sudo docker run -d --network host --privileged -v $HOME:/home/$USER --mount source=kalivol,target=/opt/vol/ --name 'kalidesktop' kalidesktop
 
