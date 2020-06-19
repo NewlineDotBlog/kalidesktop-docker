dd9f8394e2e9f92035461a07ca6b0609be3e25f9
Newline <JordanPatel@pm.me>
2020-06-19 22:33:24 +0200

    Added comments and replaced vnc connector

--- buildkali.sh
+++ buildkali.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 
+# This script is meant to automate building the Dockerfile present in the `kalidesktopdocker/` folder. Useful for testing purposes.
+# If you simply want to get started quickly, consider looking at `pullkali.sh` instead.
+
 echo 'Creating volume...'
 sudo docker volume create kalivol
-
-# Side not, we're using the "whoami" command instead of leveraging the $HOME variable to ensure
-# we don't clash with any weird home directories in the kali machine
-USER=`whoami` # Cannot do this inline in a sudo command, will return root instead
-
-# --mount source=kalivol,target=/ \
 
 echo 'Building image, this might take a while...'
 sudo docker build -t kalidesktop kalidesktopdocker/  # | grep -E 'Progress|Step' # Print out only progress information
 
 echo 'Finished building! Running kali and waiting...'
-sudo docker run -d --network host --privileged -v $HOME:/home/$USER --mount source=kalivol,target=/opt/vol/ kalidesktop # | grep Progress &
+sudo docker run -d --network host --privileged -v $HOME:/home/`whoami` --mount source=kalivol,target=/opt/vol/ kalidesktop # | grep Progress &
 sleep 30
 
 unset USER
@@ -23,7 +20,8 @@ select CHOICE in vnc novnc ssh none ; do
 
   case $CHOICE in
     vnc)
-	(command -v xtigervncviewer &>/dev/null && xtigervncviewer 127.0.0.1:5900 -Fullscreen &>/dev/null & ) || (echo 'xtigervnc not found... Installing...'; sudo apt-get update &>/dev/null && sudo apt-get install tigervnc-viewer --show-progress | grep Progress && xtigervncviewer 127.0.0.1:5900 -Fullscreen &>/dev/null ) || echo 'Failed to install/run xtigervncviewer... Please point your favourite VNC viewer at localhost:5900' &
+	echo 'Opening your default vnc viewer'
+	xdg-open vnc://localhost:5900 & || echo 'Opening vnc viewer failed. Please install a vnc viewer and connect to localhost:5900'
         ;;
     novnc)
 	echo 'Opening your default browser...'
